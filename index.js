2d74bbc63c272784e032934e369e29a4f1ae52b2
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-03 20:15:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names){
+    let messages=[];
+    for(let i=0;i< names.length;i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }   
+    return messages;
+}
+function countDown(n){
+    while(n>0){
+        console.log(n)
+        n--;
+    }
+    console.log(n)
+}
+
